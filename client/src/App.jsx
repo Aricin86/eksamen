d5e933ddcf39b0a47374fd9f92bfab2efee877fb
Marte Olavsdatter <marte.olavsdatter@gmail.com>
2020-12-02 15:09:55 +0100

    React setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import GlobalStyle from './styles/GlobalStyles';
+import Routes from './routes/Routes';
 
-const App = () => <div>Hei hei</div>;
+const App = () => (
+  <>
+    <GlobalStyle />
+    <Routes />
+  </>
+);
 
 export default App;
