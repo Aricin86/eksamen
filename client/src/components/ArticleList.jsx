91241e09c7bd1b685ca78d7767963208e0672457
Marte Olavsdatter <marte.olavsdatter@gmail.com>
2020-12-09 15:03:13 +0100

    Bug fix

--- client/src/components/ArticleList.jsx
+++ client/src/components/ArticleList.jsx
@@ -42,7 +42,7 @@ const ArticleList = () => {
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
-      const { data } = await articleList();
+      const { data, error } = await articleList();
       if (!data.success) {
         setError(error);
       } else {
@@ -52,7 +52,7 @@ const ArticleList = () => {
       setLoading(false);
     };
     fetchData();
-  });
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
