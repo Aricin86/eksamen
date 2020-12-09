636d2b9327e6e4c99ef1adf9d6e7a80cc5a4908f
Marte Olavsdatter <marte.olavsdatter@gmail.com>
2020-12-09 13:54:10 +0100

    Styling

--- client/src/components/ArticleDetailedView.jsx
+++ client/src/components/ArticleDetailedView.jsx
@@ -1,35 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  StyledArticleDetailed,
+  Button,
+  RegisterButton,
+  ArticleDate,
+  ArticleAuthor,
+  ArticleDetailedHeader,
+} from '../styled/Styled';
 
-const ArticleDetailedView = ({ error, loading, article }) => {
+const ArticleDetailedView = ({ error, loading, article }) => (
   // const [error, setError] = useState('');
 
-  const handleDelete = () => {
-    // validateRegistrationForm();
-    // Fiks sletting
-  };
+  // const handleDelete = () => {
+  //   // validateRegistrationForm();
+  //   // Fiks sletting
+  // };
 
-  const handleEdit = () => {
-    // validateRegistrationForm();
-    // Link til ArticleForm og send med id (eller hele artikkelen)
-  };
+  // const handleEdit = () => {
+  //   // validateRegistrationForm();
+  //   // Link til ArticleForm og send med id (eller hele artikkelen)
+  // };
 
-  return (
-    <section>
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {!article && (
-        <p>
-          Artikkelen kunne ikke hentest fra databasen eller finnes ikke lenger.
-        </p>
-      )}
-      <p>{article.author}</p>
-      <p>{article.createdAt}</p>
-      <p>{article.ingress}</p>
-      <p>{article.content}</p>
-      <button type="button">Slett</button>
-      <button type="button">Rediger</button>
-    </section>
-  );
+  // const FormatDate = () => article.createdAt.substring(0, 9);
+
+  <StyledArticleDetailed>
+    {loading && <p>Loading...</p>}
+    {error && <p>{error}</p>}
+    {!article && (
+      <p>
+        Artikkelen kunne ikke hentest fra databasen eller finnes ikke lenger.
+      </p>
+    )}
+    <ArticleDetailedHeader>
+      <ArticleAuthor>Av {article.author}</ArticleAuthor>
+      <ArticleDate>
+        {/* <FormatDate /> */}
+        {article.createdAt}
+      </ArticleDate>
+    </ArticleDetailedHeader>
+    {/* <p>{article.ingress}</p> */}
+    <p>{article.content}</p>
+    <Button type="button">Slett</Button>
+    <RegisterButton type="button">Rediger</RegisterButton>
+  </StyledArticleDetailed>
+);
+ArticleDetailedView.propTypes = {
+  error: PropTypes.any,
+  loading: PropTypes.any,
+  article: PropTypes.any,
+  createdAt: PropTypes.any,
+  // ingress: PropTypes.any,
+  content: PropTypes.any,
 };
 
 export default ArticleDetailedView;
--- client/src/components/ArticleList.jsx
+++ client/src/components/ArticleList.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import { Container, Button, StyledArticleListed } from '../styled/Styled';
+import {
+  Container,
+  Button,
+  StyledArticleListed,
+  ArticleHeader,
+} from '../styled/Styled';
 import { articleList } from '../utils/articleService';
 import { categoryList } from '../utils/categoryService';
 import { useAuthContext } from '../context/AuthProvider';
@@ -47,7 +52,7 @@ const ArticleList = () => {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -63,7 +68,7 @@ const ArticleList = () => {
 
   return (
     <Container>
-      <header>
+      <ArticleHeader>
         {isLoggedIn && isAdmin && (
           <Button as={NavLink} to="/ny-artikkel">
             Ny artikkel
@@ -78,7 +83,7 @@ const ArticleList = () => {
               </option>
             ))}
         </select>
-      </header>
+      </ArticleHeader>
 
       <section>
         {error && <p>{error}</p>}
